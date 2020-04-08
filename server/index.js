f36d1457e34033d5bb4aca5991e2c48009709c79
Gabriel Pasquale <pasqualeunq@gmail.com>
2020-04-08 17:12:24 -0300

    join refactored

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const socketio = require('socket.io');
-const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
+const { addUser, removeUser, getUser, getUserById, getUsersInRoom } = require('./users');
 const router = require('./router');
 
 const app = express();
@@ -13,15 +13,14 @@ const io = socketio(server);
 
 io.on('connect', (socket) => {
   socket.on('join', ({ name, room }, callback) => {
-		if(!name || !room) return callback({ error: 'Username and room are required.' });
+    if(!name || !room) return callback({ error: 'Username and room are required.' });
 
     name = name.trim().toLowerCase();
     room = room.trim().toLowerCase();
 
-    const { error } = addUser({ id: socket.id, name, room });
+    if(getUser(name, room)) return callback({ error: 'Username is taken.' });
 
-    if(error) return callback(error);
-
+    addUser({ id: socket.id, name, room });
     socket.join(room);
 
     socket.emit('message', { user: 'admin', text: `${name}, welcome to room ${room}.`});
@@ -33,7 +32,7 @@ io.on('connect', (socket) => {
   });
 
   socket.on('sendMessage', (message, callback) => {
-    const user = getUser(socket.id);
+    const user = getUserById(socket.id);
     io.to(user.room).emit('message', { user: user.name, text: message });
     callback();
   });
@@ -45,8 +44,8 @@ io.on('connect', (socket) => {
   socket.on('disconnect', () => {
     const user = removeUser(socket.id);
     if(user) {
-			const { name, room } = user
-			socket.leave(room)
+			const { name, room } = user;
+			socket.leave(room);
       io.to(room).emit('message', { user: 'Admin', text: `${name} has left.` });
 			io.to(room).emit('roomData', { room: room, users: getUsersInRoom(room) });
 			io.to(room).emit('typing', { name: name, room: room, value: '' });
--- server/users.js
+++ server/users.js
@@ -1,18 +1,14 @@
 const users = [];
 
-exports.addUser = ({ id, name, room }) => {
-  const existingUser = users.find((user) => user.room === room && user.name === name);
-  if(existingUser) return { error: 'Username is taken.' };
-  const user = { id, name, room };
-  users.push(user);
-  return { user };
-};
+exports.getUser = (name, room) => users.find(user => user.name === name && user.room === room);
 
-exports.removeUser = (id) => {
-  const index = users.findIndex((user) => user.id === id);
+exports.getUserById = id => users.find(user => user.id === id);
+
+exports.addUser = user => users.push(user);
+
+exports.removeUser = id => {
+  const index = users.findIndex(user => user.id === id);
   if(index !== -1) return users.splice(index, 1)[0];
 };
 
-exports.getUser = (id) => users.find((user) => user.id === id);
-
-exports.getUsersInRoom = (room) => users.filter((user) => user.room === room);
+exports.getUsersInRoom = room => users.filter(user => user.room === room);
